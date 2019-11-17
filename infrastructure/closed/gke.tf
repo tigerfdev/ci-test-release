7d08be0fa5fddb8fe7cec5ef9f5fdce75b7e2e9a
tiger <tiger.fdev@gmail.com>
2019-11-17 17:55:50 +0900

    create closed folder

---
+++ infrastructure/closed/gke.tf
@@ -0,0 +1,37 @@
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = var.location
+
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "my-gke-node-pool"
+  location   = var.location
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  management {
+    auto_repair = true
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
